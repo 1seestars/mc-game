2b724c378992d141c64057e2ca1180bfd340aae4
Vitaly Matyah <vitalik.matyah@gmail.com>
2020-12-26 23:31:02 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react'
 
-function App() {
+const App = () => {
+  const [selectedCharacterId, setSelectedCharacterId] = useState(0)
+  const [characterList, setCharacterList] = useState(
+    Array(28).fill({
+      name: 'test'
+    })
+  )
+
+  useEffect(() => {
+    ulRef.current.focus()
+  }, [])
+
+  const ulRef = useRef({})
+
+  const handleListKeyDown = (e) => {
+    if (e.keyCode === 39) {
+      if (selectedCharacterId < characterList.length - 1) {
+        setSelectedCharacterId(selectedCharacterId + 1)
+      }
+    }
+
+    if (e.keyCode === 37) {
+      if (selectedCharacterId > 0) {
+        setSelectedCharacterId(selectedCharacterId - 1)
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <ul
+        onKeyDown={handleListKeyDown}
+        style={{ background: 'lightgray', outline: 'none' }}
+        tabIndex={0}
+        ref={ulRef}
+      >
+        {characterList.map((character, index) => (
+          <li key={index} onClick={(e) => setSelectedCharacterId(index)}>
+            {character.name}
+            {selectedCharacterId === index && (
+              <span>
+                <strong>&nbsp; selected</strong>
+              </span>
+            )}
+          </li>
+        ))}
+      </ul>
+    </>
+  )
 }
 
-export default App;
+export default App
